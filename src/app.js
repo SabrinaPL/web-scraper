d59ed58524167d6415bf9886fd63ad0383b37f24
Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
2023-12-16 08:11:57 +0100

    Added some starting files, folders and dependencies to project

---
+++ src/app.js
@@ -0,0 +1,6 @@
+/**
+ * Main application file.
+ *
+ * @author Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
+ * @version 1.0.0
+ */
---
+++ src/web-scraper.js
@@ -0,0 +1,6 @@
+/**
+ * Web Scraper module.
+ *
+ * @author Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
+ * @version 1.0.0
+ */
