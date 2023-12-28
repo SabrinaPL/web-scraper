0d61d4209ec973a79b0bd1c4aa0cee26853870ba
Sabrina Prichard-Lybeck <sp223kz@student.lnu.se>
2023-12-28 16:12:09 +0100

    Code clean up and refactoring

--- src/orchestrator.js
+++ src/orchestrator.js
@@ -11,28 +11,57 @@ import { WebScraper } from './web-scraper.js'
  * @class
  */
 export class Orchestrator {
+  #url = ''
+
+  /**
+   * Creates an instance of the Web Scraper.
+   *
+   *
+   */
+  constructor () {
+    this.scraper = new WebScraper()
+  }
+
   /**
    * Method to start and orchestrate application process.
    *
+   * @async
    * @function
    * @param {string} url - The URL to scrape.
    */
   async start (url) {
-    const scraper = new WebScraper()
-    const href = await scraper.findUrls(url)
-    console.log(href)
-
-    const calendarLinks = await scraper.findUrls(href[0])
-
-    console.log(calendarLinks)
-
-    const availableDates = []
-
-    for (let i = 0; i < calendarLinks.length; i++) {
-      const calendarLinkAbsolute = await scraper.createAbsoluteUrl(href[0], calendarLinks[i])
-      availableDates.push(await scraper.findAvailableDates(calendarLinkAbsolute))
+    try {
+      this.#url = url
+      await this.scrapeData()
+    } catch (error) {
+      console.log(error)
     }
+  }
 
-    console.log(availableDates)
+  /**
+   * Method to scrape data.
+   *
+   * @async
+   * @function
+   */
+  async scrapeData () {
+    try {
+      const href = await this.scraper.findUrls(this.#url)
+
+      const calendarLinks = await this.scraper.findUrls(href[0])
+
+      console.log('Scraping links... OK')
+
+      const availableDates = []
+
+      for (let i = 0; i < calendarLinks.length; i++) {
+        const calendarLinkAbsolute = await this.scraper.createAbsoluteUrl(href[0], calendarLinks[i])
+        availableDates.push(await this.scraper.findAvailableDates(calendarLinkAbsolute))
+      }
+
+      console.log('Scraping dates... OK')
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
